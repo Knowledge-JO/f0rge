025b74470de0b577ba1f3fb88fba0ca8f5177015
yomidep <noabiliaminfirst@gmail.com>
2023-12-21 18:05:24 +0100

    nft added

--- contract/contracts/maker20.sol
+++ contract/contracts/maker20.sol
@@ -2,9 +2,16 @@
 pragma solidity >=0.8.0 <=0.8.20;
 
 import "./token.sol";
+import "./nft.sol";
 
 contract Maker20 {
+    ERC1155Token[] public tokens; //
+    mapping(uint => address) public indexToContract; //
+    mapping(uint => address) public indexToOwner; //
+
     event TokenCreated(address indexed tokenAddress, string name, string symbol, uint256 totalSupply);
+    event ERC1155Created(address owner, address tokenContract); //
+    event ERC1155Minted(address owner, address tokenContract, uint amount); //
 
     function createToken(string memory name, string memory symbol, uint256 totalSupply, uint _holdingCap) external {
         token newToken = new token(name, symbol, totalSupply, _holdingCap);
@@ -31,6 +38,24 @@ contract Maker20 {
 
     function sellTax() {
         
+    }
+
+    function mintERC1155( uint _index, string memory _name, uint256 amount) public {
+
+        uint id = getIdByName(_index, _name);
+        tokens[_index].mint(indexToOwner[_index], id, amount);
+        emit ERC1155Minted(tokens[_index].owner(), address(tokens[_index]), amount);
+    }
+
+
+    function getCountERC1155byIndex(uint256 _index, uint256 _id) public view returns (uint amount) {
+        return tokens[_index].balanceOf(indexToOwner[_index], id);
+    }
+
+    function getCountERC1155byName(uint256 _index, string calldata _name) public view returns (uint amount) 
+    {
+        uint id = getIdByName(_index, _name);
+        return tokens[_index].balanceOf(indexToOwner[_index], id);
     }
     
 }
