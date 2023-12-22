93c20a43a2db9e26b249b963614f3bca251f1b0c
Knowledge-JO <megamindtheincredible@gmail.com>
2023-12-22 21:56:19 +0100

    updated contracts

--- contract/token.sol
+++ contract/token.sol
@@ -1,40 +1,116 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <=0.8.20;
+pragma solidity ^0.8.20;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract token is ERC20 {
-    address public owner;
+contract Register {
+    function register(address _recipient) public returns (uint256 tokenId) {}
+}
+
+contract Token is ERC20, Ownable {
+    address public s_owner;
     uint holdingCapinPercent;
+    uint256 private s_buyTax;
+    uint256 private s_sellTax;
+    bool private s_salesActive;
+    address private s_taxWallet;
+    uint8 private s_tokenDecimals;
 
-    constructor(string memory name, string memory symbol, uint256 totalSupply, uint _holdingCap) ERC20(name, symbol) {
+    mapping (address => bool ) private automatedMarketMakerPairs;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply,
+        uint _holdingCap,
+        uint256 _buyTax,
+        uint _sellTax,
+        uint8 _tokenDecimals,
+        address _ownerAddress,
+        address _modeAddress
+    ) ERC20(name, symbol) Ownable(_ownerAddress) {
         holdingCapinPercent = _holdingCap;
-        _mint(msg.sender, totalSupply * (10**uint256(decimals())));
+        s_buyTax = _buyTax;
+        s_sellTax = _sellTax;
+        s_tokenDecimals = _tokenDecimals;
+        Register sfsContract = Register(_modeAddress); //instance of SFS contract
+        sfsContract.register(_ownerAddress); // register this contract and send NFT to owneraddress to claim fees.
+        _mint(msg.sender, totalSupply * (10 ** uint256(decimals())));
     }
 
-    function getMaxHoldings() public view returns(uint256) {
+    function decimals() public view virtual override returns(uint8 newDecimals){
+        newDecimals = s_tokenDecimals;
+        if(newDecimals > 0){
+            return newDecimals;
+        }else {
+            return 18;
+        }
+    }
+
+    function getMaxHoldings() public view returns (uint256) {
         if (holdingCapinPercent == 0) {
             // If holding cap is set to 0, return maximum possible value
             return type(uint256).max;
         } else {
-            return (totalSupply() * holdingCapinPercent / 100);
+            return ((totalSupply() * holdingCapinPercent) / 100);
         }
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal override virtual  {
-        require(balanceOf(recipient) + amount <= getMaxHoldings(), "Recipient exceeds holding cap");
-        super._transfer(sender, recipient, amount);
+    function setBuyTax (uint256 _newBuyTax) public onlyOwner {
+        require(_newBuyTax < s_buyTax, "new tax cannot exceed previous tax"); 
+        s_buyTax = _newBuyTax;
     }
 
-    function _approve(address _owner, address spender, uint256 amount) internal override virtual {
-        owner = _owner;
-        require(balanceOf(spender) + amount <= getMaxHoldings(), "Spender exceeds holding cap");
-        super._approve(owner, spender, amount);
+    function setSellTax (uint256 _newSellTax) public onlyOwner {
+        require(_newSellTax < s_sellTax, "new tax cannot exceed previous tax"); 
+        s_sellTax = _newSellTax;
     }
 
-    function _transferFrom(address sender, address recipient, uint256 amount) internal  virtual  {
-        require(balanceOf(recipient) + amount <= getMaxHoldings(), "Recipient exceeds holding cap");
-        _transferFrom(sender, recipient, amount);
+    function setAutomatedMarketMarkerPair(address pair, bool value) public onlyOwner {
+        automatedMarketMakerPairs[pair] = value;
     }
-    
+
+    function setTaxWallet(address _newTaxWallet) public onlyOwner {
+        s_taxWallet = _newTaxWallet;
+    }
+
+    function flipSalesState () public onlyOwner {
+        s_salesActive = !s_salesActive;
+    }
+
+    function getSalesState() public view returns(bool){
+        return s_salesActive;
+    }
+
+    function getTax() public view returns(uint256, uint256){
+        return (s_buyTax, s_sellTax);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override {
+        require(s_salesActive, "sales not open yet");
+        require(s_taxWallet != address(0), "tax wallet is zero address, set tax wallet");
+
+        // check if max holdings exceeded
+        if(automatedMarketMakerPairs[from]){
+            require(balanceOf(to) + value <= getMaxHoldings(), "You've exceeded the amount of token you can purchase");
+        }
+
+        // remove buy tax if "from" is pair address
+        uint256 fees;
+        if(automatedMarketMakerPairs[from] && s_buyTax > 0){
+            fees = value * s_buyTax / 100;
+        }
+        // remove sell tax if "to" is pair address
+        else if (automatedMarketMakerPairs[to] && s_sellTax > 0){
+            fees = value * s_sellTax / 100;
+        }
+
+        if(fees > 0) {
+            super._update(from, s_taxWallet, fees);
+            value -= fees;
+        }
+
+        super._update(from, to, value);
+    }
 }
