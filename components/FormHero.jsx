4272813e96454fdcf32fd35caaaab4079c3f2bcf
Knowledge-JO <megamindtheincredible@gmail.com>
2023-12-29 00:49:12 +0100

    changed mode to fons

--- components/FormHero.jsx
+++ components/FormHero.jsx
@@ -8,7 +8,7 @@ const FormHero = () => {
           Deploy token on{" "}
           <span
             className="block overflow-hidden text-[20px] lg:text-[25px] text-red-500 font-bold relative my-[5px] mx-0  after:absolute after:left-0 after:bottom-0 after:h-[100%] after:w-[100%] after:bg-black after:border-l-2 after:border-red-500 after:animate-typing_anim "
-          >Mode Blockchain</span>
+          >FonSmartChain</span>
         </h1>
       </div>
       <p className="text-center mt-3">
